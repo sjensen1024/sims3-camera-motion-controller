6044d6b3f62e9dba61643a361c32fd9ebe5a009b
sjensen1024 <stevenmjensen1024@gmail.com>
2025-08-17 09:23:10 -0500

    start consolidating existing movement tracker class and add test framework

--- src/camera/movement_tracker.py
+++ src/camera/movement_tracker.py
@@ -18,45 +18,38 @@ class MovementTracker:
         self.is_turning_right = False
 
     def start_moving_forward(self):
-        if self.is_moving_forward: return
-        self.stop_moving_backward()
-        self.key_operator.press_keys(KeySet.MOVE_FORWARD.value)
-        self.is_moving_forward = False
+        self.__process_standard_start_movement('is_moving_forward', KeySet.MOVE_FORWARD.value, ['stop_moving_backward'])
 
     def stop_moving_forward(self):
-        if not self.is_moving_forward: return
-        self.key_operator.release_keys(KeySet.MOVE_FORWARD.value)
-        self.is_moving_forward = True
+        self.__process_standard_stop_movement('is_moving_forward', KeySet.MOVE_FORWARD.value)
 
     def start_moving_backward(self):
-        if self.is_moving_backward: return
-        self.stop_moving_forward()
-        self.key_operator.press_keys(KeySet.MOVE_BACKWARD.value)
-        self.is_moving_backward = False
+        self.__process_standard_start_movement('is_moving_backward', KeySet.MOVE_BACKWARD.value, ['stop_moving_forward'])
 
     def stop_moving_backward(self):
-        if not self.is_moving_backward: return
-        self.key_operator.release_keys(KeySet.MOVE_BACKWARD.value)
-        self.is_moving_barckward = True
+        self.__process_standard_stop_movement('is_moving_backward', KeySet.MOVE_BACKWARD.value)
 
     def start_moving_left(self):
-        if self.is_moving_left: return
-        self.stop_moving_right()
-        self.key_operator.press_keys(KeySet.MOVE_LEFT.value)
-        self.is_moving_left = True
+        self.__process_standard_start_movement('is_moving_left', KeySet.MOVE_LEFT.value, ['stop_moving_right'])
 
     def stop_moving_left(self):
-        if not self.is_moving_left: return
-        self.key_operator.release_keys(KeySet.MOVE_LEFT.value)
-        self.is_moving_left = False
+        self.__process_standard_stop_movement('is_moving_left', KeySet.MOVE_LEFT.value)
 
     def start_moving_right(self):
-        if self.is_moving_right: return
-        self.stop_moving_left()
-        self.key_operator.press_keys(KeySet.MOVE_RIGHT.value)
-        self.is_moving_right = True
+        self.__process_standard_start_movement('is_moving_right', KeySet.MOVE_RIGHT.value, ['stop_moving_left'])
 
     def stop_moving_right(self):
-        if not self.is_moving_right: return
-        self.key_operator.release_keys(KeySet.MOVE_RIGHT.value)
-        self.is_moving_right = False
+        self.__process_standard_stop_movement('is_moving_right', KeySet.MOVE_RIGHT.value)
+
+    def __process_standard_start_movement(self, attribute_name, key_set_to_press, impossible_movements_to_stop = []):
+        if getattr(self, attribute_name): return
+        for impossible_movement_to_stop in impossible_movements_to_stop:
+            method = getattr(self, impossible_movement_to_stop)
+            method()
+        self.key_operator.press_keys(key_set_to_press)
+        setattr(self, attribute_name, True)
+
+    def __process_standard_stop_movement(self, attribute_name, key_set_to_release):
+        if not getattr(self, attribute_name): return
+        self.key_operator.release_keys(key_set_to_release)
+        setattr(self, attribute_name, False)
---
+++ tests/camera/test_movement_tracker.py
@@ -0,0 +1,105 @@
+import unittest
+from unittest.mock import MagicMock
+from src.input.key_operator import KeyOperator
+from src.input.key_set import KeySet
+from tests.support.test_util import TestUtil
+from src.camera.movement_tracker import MovementTracker
+
+class TestMovementTracker(unittest.TestCase):
+    def setUp(self):
+        self.movement_tracker = MovementTracker()
+        self.test_util = TestUtil()
+        self.original_press_keys = KeyOperator.press_keys
+        self.original_release_keys = KeyOperator.release_keys
+        KeyOperator.press_keys = MagicMock("test_movement_tracker__press_keys")
+        KeyOperator.release_keys = MagicMock("test_movement_tracker__release_keys")
+
+    def tearDown(self):
+        KeyOperator.press_keys = self.original_press_keys
+        KeyOperator.release_keys = self.original_release_keys
+
+    def test_when_init_given_checking_key_operator_then_should_be_not_none(self):
+        self.assertIsNotNone(self.movement_tracker.key_operator)
+
+    def test_when_init_given_checking_is_moving_forward_then_should_be_false(self):
+        self.assertFalse(self.movement_tracker.is_moving_forward)
+
+    def test_when_init_given_checking_is_moving_backward_then_should_be_false(self):
+        self.assertFalse(self.movement_tracker.is_moving_backward)
+
+    def test_when_init_given_checking_is_moving_left_then_should_be_false(self):
+        self.assertFalse(self.movement_tracker.is_moving_left)
+
+    def test_when_init_given_checking_is_moving_right_then_should_be_false(self):
+        self.assertFalse(self.movement_tracker.is_moving_right)
+
+    def test_when_init_given_checking_is_raising_then_should_be_false(self):
+        self.assertFalse(self.movement_tracker.is_raising)
+
+    def test_when_init_given_checking_is_lowering_then_should_be_false(self):
+        self.assertFalse(self.movement_tracker.is_lowering)
+
+    def test_when_init_given_checking_is_zooming_in_then_should_be_false(self):
+        self.assertFalse(self.movement_tracker.is_zooming_in)
+
+    def test_when_init_given_checking_is_zooming_out_then_should_be_false(self):
+        self.assertFalse(self.movement_tracker.is_zooming_out)
+
+    def test_when_init_given_checking_is_rotating_left_then_should_be_false(self):
+        self.assertFalse(self.movement_tracker.is_rotating_left)
+
+    def test_when_init_given_checking_is_rotating_right_then_should_be_false(self):
+        self.assertFalse(self.movement_tracker.is_rotating_right)
+
+    def test_when_init_given_checking_is_turning_left_then_should_be_false(self):
+        self.assertFalse(self.movement_tracker.is_turning_left)
+
+    def test_when_init_given_checking_is_turning_right_then_should_be_false(self):
+        self.assertFalse(self.movement_tracker.is_turning_right)
+
+    def test_when_start_moving_forward_given_is_not_already_moving_forward_then_should_start_moving_forward_and_stop_moving_backward(self):
+        self.__test_start_movement_when_not_started_yet("start_moving_forward", "is_moving_forward", KeySet.MOVE_FORWARD.value, "stop_moving_backward")
+
+    def test_when_start_moving_forward_given_is_already_moving_forward_then_should_keep_moving_forward_without_doing_anything_else(self):
+        self.__test_start_movement_when_already_started("start_moving_forward", "is_moving_forward", "stop_moving_backward")
+
+    def test_when_stop_moving_forward_given_is_moving_forward_then_should_stop_moving_forward(self):
+        self.__test_stop_movement_when_started("stop_moving_forward", "is_moving_forward", KeySet.MOVE_FORWARD.value)
+
+    def test_when_stop_moving_forward_given_is_not_moving_forward_yet_then_should_stay_not_moving_forward(self):
+         self.__test_stop_movement_when_not_started_yet("stop_moving_forward", "is_moving_forward")
+
+    def __test_start_movement_when_not_started_yet(self, start_movement_method_name, movement_attribute_name, expected_key_set, countermovement_method_name):
+        countermovement_method = getattr(self.movement_tracker, countermovement_method_name)
+        self.original_countermovement_method = countermovement_method
+        setattr(self.movement_tracker, countermovement_method_name, MagicMock("test_movement_tracker__" + countermovement_method_name))
+        start_movement_method = getattr(self.movement_tracker, start_movement_method_name)
+        start_movement_method()
+        getattr(self.movement_tracker, countermovement_method_name).assert_called()
+        self.test_util.assert_called_with_args_x_times(KeyOperator.press_keys, [expected_key_set], 1)
+        self.assertTrue(getattr(self.movement_tracker, movement_attribute_name))
+        setattr(self.movement_tracker, countermovement_method_name, self.original_countermovement_method)
+
+    def __test_start_movement_when_already_started(self, start_movement_method_name, movement_attribute_name, countermovement_method_name):
+        setattr(self.movement_tracker, movement_attribute_name, True)
+        countermovement_method = getattr(self.movement_tracker, countermovement_method_name)
+        self.original_countermovement_method = countermovement_method
+        setattr(self.movement_tracker, countermovement_method_name, MagicMock("test_movement_tracker__" + countermovement_method_name))
+        start_movement_method = getattr(self.movement_tracker, start_movement_method_name)
+        start_movement_method()
+        getattr(self.movement_tracker, countermovement_method_name).assert_not_called()
+        KeyOperator.press_keys.assert_not_called()
+        self.assertTrue(getattr(self.movement_tracker, movement_attribute_name))
+
+    def __test_stop_movement_when_started(self, stop_movement_method_name, movement_attribute_name, expected_key_set):
+        setattr(self.movement_tracker, movement_attribute_name, True)
+        stop_movement_method = getattr(self.movement_tracker, stop_movement_method_name)
+        stop_movement_method()
+        self.test_util.assert_called_with_args_x_times(KeyOperator.release_keys, [expected_key_set], 1)
+        self.assertFalse(getattr(self.movement_tracker, movement_attribute_name))
+
+    def __test_stop_movement_when_not_started_yet(self, stop_movement_method_name, movement_attribute_name):
+        stop_movement_method = getattr(self.movement_tracker, stop_movement_method_name)
+        stop_movement_method()
+        KeyOperator.release_keys.assert_not_called()
+        self.assertFalse(getattr(self.movement_tracker, movement_attribute_name))
