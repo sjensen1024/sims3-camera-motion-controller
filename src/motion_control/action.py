57d09b565341ed6b6fbd7073c4cea3765b814391
sjensen1024 <stevenmjensen1024@gmail.com>
2025-08-22 20:31:30 -0500

    added motion control action class and test

---
+++ src/motion_control/action.py
@@ -0,0 +1,10 @@
+class Action:
+    def __init__(self, run_at_this_many_seconds_in, caller, method_name):
+        self.run_at_this_many_seconds_in = run_at_this_many_seconds_in
+        self.caller = caller
+        self.method_name = method_name
+    
+    def run(self):
+        method_to_call = getattr(self.caller, self.method_name)
+        method_to_call()
+        
---
+++ tests/motion_control/test_action.py
@@ -0,0 +1,52 @@
+import unittest
+from unittest.mock import MagicMock
+from tests.support.test_util import TestUtil
+from src.camera.camera_operator import CameraOperator
+from src.camera.movement_tracker import MovementTracker
+from src.motion_control.action import Action
+
+class TestMotionControlAction(unittest.TestCase):
+    def setUp(self):
+        self.test_util = TestUtil()
+        self.original_camera_operator_power_on = CameraOperator.power_on
+        CameraOperator.power_on = MagicMock("test_action__camera_operator_power_on")
+        self.original_movement_tracker_start_moving_forward = MovementTracker.start_moving_forward
+        MovementTracker.start_moving_forward = MagicMock("test_action__movement_tracker_start_moving_forward")
+        self.test_camera_operator = CameraOperator()
+        self.test_camera_operator_action = Action(0.5, self.test_camera_operator, "power_on")
+        self.test_movement_tracker = MovementTracker()
+        self.test_movement_tracker_action = Action(1.5, self.test_movement_tracker, "start_moving_forward")
+
+    def tearDown(self):
+        CameraOperator.power_on = self.original_camera_operator_power_on
+        MovementTracker.start_moving_forward = self.original_movement_tracker_start_moving_forward
+
+    def test_suite_for_camera_operator_action(self):
+       self.__assert_action_is_initialized_correctly(
+           self.test_camera_operator_action, 
+           0.5, 
+           self.test_camera_operator, 
+           "power_on"
+        )
+       self.__assert_run_calls_method_from_caller(self.test_camera_operator_action)
+
+    def test_suite_for_movement_tracker_action(self):
+        self.__assert_action_is_initialized_correctly(
+            self.test_movement_tracker_action, 
+            1.5, 
+            self.test_movement_tracker, 
+            "start_moving_forward"
+        )
+        self.__assert_run_calls_method_from_caller(self.test_movement_tracker_action)
+
+
+    def __assert_action_is_initialized_correctly(self, action, expected_seconds_in, expected_caller, expected_caller_method):
+        self.assertEqual(action.run_at_this_many_seconds_in, expected_seconds_in)
+        self.assertEqual(action.caller, expected_caller)
+        self.assertEqual(action.method_name, expected_caller_method)
+
+    def __assert_run_calls_method_from_caller(self, action):
+        action.run()
+        expected_call = getattr(action.caller, action.method_name)
+        expected_call.assert_called()
+        
